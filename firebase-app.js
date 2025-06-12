c185cb3c31bf94e4b7e1ac98be6d8e03f7f10842
JoshiRudra-devop <rudrajoshi1612@gmail.com>
2025-06-12 23:31:29 +0530

    Create firebase-app.js

---
+++ firebase-app.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBDnGhIbPuy-QfXr06BoK1pq2F1nyTeBkw",
+  authDomain: "authentication-1612.firebaseapp.com",
+  projectId: "authentication-1612",
+  storageBucket: "authentication-1612.firebasestorage.app",
+  messagingSenderId: "295810203713",
+  appId: "1:295810203713:web:eda56aa73c0bbf6e0a46c0",
+  measurementId: "G-HSHX6XTEDS"
+};
+firebase.initializeApp(firebaseConfig);
